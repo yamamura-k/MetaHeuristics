4c586119d0c28ebfeee3022c8946ca647e47b4c7
Keiichiro Yamamura <59185811+yamamura-k@users.noreply.github.com>
2021-07-02 23:50:15 +0900

    Delete __init__.py

--- metaheuristics_with_numba/__init__.py
+++
@@ -1,5 +0,0 @@
-from .parallel import paraABC as paraABC
-from .parallel import paraBA as paraBA
-from .sequential import ABC as ABC
-from .sequential import BA as BA
-from .sequential import GWO as GWO
