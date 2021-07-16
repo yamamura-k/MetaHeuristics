c05d1624f3aa8de5fd1254f49137b55e3ec90142
yamamura-k <yamayama23bb@gmail.com>
2021-07-16 22:49:24 +0900

    add docstring

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -6,6 +6,34 @@ import algorithm.nelder_mead as nelder_mead
 
 
 def optimize(dimension, f, max_iter, algo="CG", maximize=False, *args, **kwargs):
+    """[summary]
+
+    Parameters
+    ----------
+    dimension : int
+        number of variables
+    f : callable
+        objective function
+    max_iter : int
+        maximum number of iteration
+    algo : str, optional
+        algorithm name, by default "CG". [ABC, BA, GWO, paraABC, paraBA, GD, CG, NV, NW, NM] is valid.
+    maximize : bool, optional
+        When you want to solve maximization problem, it should be True, by default False
+    options : dict, optional
+        algorithm specific parameters.
+        Coming soon...
+
+    Returns
+    -------
+    ResultManager
+        result of optimization problem
+
+    Raises
+    ------
+    NotImplementedError
+        [description]
+    """
     f = FunctionWrapper(f, maximize=maximize, *args, **kwargs)
     if algo == "ABC":
         return metaheuristics.ABC.minimize(dimension, f, max_iter, *args, **kwargs)
